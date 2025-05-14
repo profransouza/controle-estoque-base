b9ff3a7b2dc126d491c08eaec06f5c95aab90db2
Francisco Souza <francisco.caldeira@educacao.mg.gov.br>
2025-05-14 20:33:50 -0300

    Iniciado Servidor Node.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+//Declaracao de variaveis
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+// Inicia o servidor express
+const app = express();
+
+// Servindo arquivos estaticos
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ src/public/teste.js
@@ -0,0 +1,4 @@
+const nome = "Fulano";
+const idade = 30;
+
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`);
